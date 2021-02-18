02bd86b7745cea7d9dbbd2d679d602b44b6ea58a
Hydrae 404 Arius <76522473+HYDRAE404@users.noreply.github.com>
2021-02-18 11:20:46 +0800

    Update firstProgram.cs

--- C#/firstProgram.cs
+++ C#/firstProgram.cs
@@ -1,1 +1,12 @@
+using System;
 
+namespace firstProgram
+{
+    class firstCode
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+        }
+    }
+}
