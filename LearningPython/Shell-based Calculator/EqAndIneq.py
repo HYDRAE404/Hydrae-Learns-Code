b9a1a7d2342e12547d9cfd662183d3be112c5f15
Hydrae 404 Arius <76522473+HYDRAE404@users.noreply.github.com>
2021-02-24 01:47:12 +0800

    Update EqAndIneq.py

--- LearningPython/Shell-based Calculator/EqAndIneq.py
+++ LearningPython/Shell-based Calculator/EqAndIneq.py
@@ -1,1 +1,28 @@
 
+def Eq(num1, num2):
+    if num1==num2:
+        result = True
+    else:
+        result = False
+    return result
+
+ def Ineq(num1, num2):
+    if num1!=num2:
+        result = True
+    else:
+        result = False
+    return result
+
+def greaterThan(num1, num2):
+    if num1 > num2:
+        result = True
+    else:
+        result = False
+    return result
+
+def lessThan(num1,num2):
+    if num1 < num2:
+        result = True
+    else:
+        result = False
+    return result
