e84d9c024b6440f590fe60eab844e41efc83f146
Hydrae 404 Arius <76522473+HYDRAE404@users.noreply.github.com>
2021-12-10 14:01:17 +0800

    Create test1.cs

---
+++ C#/test1.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace Main
+{
+    class Lobby
+    {
+        static void Main(string[] args)
+        {
+            string choice1;
+            string choice2;
+            
+            Console.Write("Would you like a cup of tea? [Yes/No]: ");
+            choice1 = Console.ReadLine();
+            
+            if (choice1 == "Yes")
+            {
+                Console.WriteLine("Well here is your cup of tea.");
+                Console.WriteLine("Now let's talk about the contract.");
+            }
+            
+            if (choice1 == "No")
+            {
+                Console.Write("Some water perhaps? [Yes/No]: ");
+                choice2 = Console.ReadLine();
+                
+                if (choice2 == "Yes")
+                {
+                    Console.WriteLine("Well here\'s some water.");
+                    Console.WriteLine("Now let's talk about the contract.");
+                }
+                
+                if (choice2 == "No")
+                {
+                    Console.WriteLine("Ok. Now let's talk about the contract.");
+                }
+            }
+        }
+    }
+}
